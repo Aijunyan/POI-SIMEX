2f647c97152ba62cc5c6483e74574f596da2cf8d
Aijunyan <97938548+Aijunyan@users.noreply.github.com>
2024-08-23 15:02:32 -0700

    Update AFT_Lognorm_Main.R

--- AFT_Lognorm_Main.R
+++ AFT_Lognorm_Main.R
@@ -1,49 +1,21 @@
 ##
-##Demonstrate the application of POI-SIMEX method using the simulated data
-##In the real application, model statement needs to be modified accordingly
+##Demonstrate the application of POI-SIMEX method using the simulated data.
+##In the real application, model statement needs to be modified accordingly.
 ###
+# REFERENCE:  "POI-SIMEX for conditionally 
+#    Poisson distributed biomarkers from tissue microarrays" (2024)
+#    Aijun Yang (aijunyan@uvic.ca), Phineas T. Hamilton, Brad H. Nelson, Julian J. Lum, 
+#    Mary Lesperance, Farouk S. Nathoo
+
 ##install required packages
 
 list.of.packages <- c("tidyverse", "survival","survivalAnalysis","SurvRegCensCov","purrr","dplyr","plyr","stringr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only = TRUE)
 
 setwd("~")
 source("./POI_SIMEX_AFT.R")
-
-lapply(list.of.packages, require, character.only = TRUE)
-
-sim.lognormT<-function(a,b,n,unif.a0=0.5,unif.b0=9,seed=12345,rcensor=0){
-  # Arguments
-  # @para a, b are gamma shape and scale parameters to simulate true covariate X
-  # @para n, the number of observations
-  # @para unif.a0, unif.b0 are two parameters of uniform distribution to simulate Z covariate
-  # @para seed: random seed
-  # @para rcensor: % of censoring, values from 0 to 1
-  # output dataset: simdata, type is dataframe
-  
-  set.seed(seed)
-  simdata<-c()
-  #area in POI process--set to 1 for simplicity
-  A<-rep(1,n)
-  ##covariate without ME
-  Z<-runif(n,unif.a0,unif.b0)
-  
-  ##a=gamma shape b=gamma scale
-  
-  lambda<-rgamma(n,shape=a,scale=b)  #rate=1/scale
-    
-    W<- rpois(n, lambda*A) #generate Poissons, mis-measured covariate
-    lambda.naive<-W/A
-    
-    log.Y<-b0+b1*lambda+b2*Z+sig.e*rnorm(n)  ##set standard normal dist rnorm(0,1)
-    Y<-exp(log.Y)
-    delta<- rep(1, n) #event indicator
-    delta[sample(n, rcensor*n)] <- 0 # random censorship
-    
-    simdata<-data.frame(Y=Y,Z=Z,lambda=lambda,lambda.naive=lambda.naive,A=A,delta=delta)
-  return (simdata)
-}
 
 
 progStart=Sys.time()
